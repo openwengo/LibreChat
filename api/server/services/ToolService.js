208e281345a4ffb21feaf4c1d68a05325d48d98d
Danny Avila <danny@librechat.ai>
2024-12-17 13:12:57 -0500

    🔧 feat: Initial MCP Support (Tools) (#5015)
    * 📝 chore: Add comment to clarify purpose of check_updates.sh script
    
    * feat: mcp package
    
    * feat: add librechat-mcp package and update dependencies
    
    * feat: refactor MCPConnectionSingleton to handle transport initialization and connection management
    
    * feat: change private methods to public in MCPConnectionSingleton for improved accessibility
    
    * feat: filesystem demo
    
    * chore: everything demo and move everything under mcp workspace
    
    * chore: move ts-node to mcp workspace
    
    * feat: mcp examples
    
    * feat: working sse MCP example
    
    * refactor: rename MCPConnectionSingleton to MCPConnection for clarity
    
    * refactor: replace MCPConnectionSingleton with MCPConnection for consistency
    
    * refactor: manager/connections
    
    * refactor: update MCPConnection to use type definitions from mcp types
    
    * refactor: update MCPManager to use winston logger and enhance server initialization
    
    * refactor: share logger between connections and manager
    
    * refactor: add schema definitions and update MCPManager to accept logger parameter
    
    * feat: map available MCP tools
    
    * feat: load manifest tools
    
    * feat: add MCP tools delimiter constant and update plugin key generation
    
    * feat: call MCP tools
    
    * feat: update librechat-data-provider version to 0.7.63 and enhance StdioOptionsSchema with additional properties
    
    * refactor: simplify typing
    
    * chore: update types/packages
    
    * feat: MCP Tool Content parsing
    
    * chore: update dependencies and improve package configurations
    
    * feat: add 'mcp' directory to package and update configurations
    
    * refactor: return CONTENT_AND_ARTIFACT format for MCP callTool
    
    * chore: bump @librechat/agents
    
    * WIP: MCP artifacts
    
    * chore: bump @librechat/agents to v1.8.7
    
    * fix: ensure filename has extension when saving base64 image
    
    * fix: move base64 buffer conversion before filename extension check
    
    * chore: update backend review workflow to install MCP package
    
    * fix: use correct `mime` method
    
    * fix: enhance file metadata with message and tool call IDs in image saving process
    
    * fix: refactor ToolCall component to handle MCP tool calls and improve domain extraction
    
    * fix: update ToolItem component for default isInstalled value and improve localization in ToolSelectDialog
    
    * fix: update ToolItem component to use consistent text color for tool description
    
    * style: add theming to ToolSelectDialog
    
    * fix: improve domain extraction logic in ToolCall component
    
    * refactor: conversation item theming, fix rename UI bug, optimize props, add missing types
    
    * feat: enhance MCP options schema with base options (iconPath to start) and make transport type optional, infer based on other option fields
    
    * fix: improve reconnection logic with parallel init and exponential backoff and enhance transport debug logging
    
    * refactor: improve logging format
    
    * refactor: improve logging of available tools by displaying tool names
    
    * refactor: improve reconnection/connection logic
    
    * feat: add MCP package build process to Dockerfile
    
    * feat: add fallback icon for tools without an image in ToolItem component
    
    * feat: Assistants Support for MCP Tools
    
    * fix(build): configure rollup to use output.dir for dynamic imports
    
    * chore: update @librechat/agents to version 1.8.8 and add @langchain/anthropic dependency
    
    * fix: update CONFIG_VERSION to 1.2.0

--- api/server/services/ToolService.js
+++ api/server/services/ToolService.js
@@ -178,6 +178,7 @@ async function processRequiredActions(client, requiredActions) {
     model: client.req.body.model ?? 'gpt-4o-mini',
     tools,
     functions: true,
+    endpoint: client.req.body.endpoint,
     options: {
       processFileURL,
       req: client.req,
@@ -386,22 +387,19 @@ async function processRequiredActions(client, requiredActions) {
  * Processes the runtime tool calls and returns the tool classes.
  * @param {Object} params - Run params containing user and request information.
  * @param {ServerRequest} params.req - The request object.
- * @param {string} params.agent_id - The agent ID.
- * @param {Agent['tools']} params.tools - The agent's available tools.
- * @param {Agent['tool_resources']} params.tool_resources - The agent's available tool resources.
+ * @param {Agent} params.agent - The agent to load tools for.
  * @param {string | undefined} [params.openAIApiKey] - The OpenAI API key.
  * @returns {Promise<{ tools?: StructuredTool[] }>} The agent tools.
  */
-async function loadAgentTools({ req, agent_id, tools, tool_resources, openAIApiKey }) {
-  if (!tools || tools.length === 0) {
+async function loadAgentTools({ req, agent, tool_resources, openAIApiKey }) {
+  if (!agent.tools || agent.tools.length === 0) {
     return {};
   }
   const { loadedTools, toolContextMap } = await loadTools({
-    user: req.user.id,
-    // model: req.body.model ?? 'gpt-4o-mini',
-    tools,
+    agent,
     functions: true,
-    isAgent: agent_id != null,
+    user: req.user.id,
+    tools: agent.tools,
     options: {
       req,
       openAIApiKey,
@@ -417,6 +415,11 @@ async function loadAgentTools({ req, agent_id, tools, tool_resources, openAIApiK
   for (let i = 0; i < loadedTools.length; i++) {
     const tool = loadedTools[i];
     if (tool.name && (tool.name === Tools.execute_code || tool.name === Tools.file_search)) {
+      agentTools.push(tool);
+      continue;
+    }
+
+    if (tool.mcp === true) {
       agentTools.push(tool);
       continue;
     }
@@ -446,10 +449,10 @@ async function loadAgentTools({ req, agent_id, tools, tool_resources, openAIApiK
   let actionSets = [];
   const ActionToolMap = {};
 
-  for (const toolName of tools) {
+  for (const toolName of agent.tools) {
     if (!ToolMap[toolName]) {
       if (!actionSets.length) {
-        actionSets = (await loadActionSets({ agent_id })) ?? [];
+        actionSets = (await loadActionSets({ agent_id: agent.id })) ?? [];
       }
 
       let actionSet = null;
@@ -485,7 +488,7 @@ async function loadAgentTools({ req, agent_id, tools, tool_resources, openAIApiK
             });
             if (!tool) {
               logger.warn(
-                `Invalid action: user: ${req.user.id} | agent_id: ${agent_id} | toolName: ${toolName}`,
+                `Invalid action: user: ${req.user.id} | agent_id: ${agent.id} | toolName: ${toolName}`,
               );
               throw new Error(`{"type":"${ErrorTypes.INVALID_ACTION}"}`);
             }
@@ -497,7 +500,7 @@ async function loadAgentTools({ req, agent_id, tools, tool_resources, openAIApiK
     }
   }
 
-  if (tools.length > 0 && agentTools.length === 0) {
+  if (agent.tools.length > 0 && agentTools.length === 0) {
     throw new Error('No tools found for the specified tool calls.');
   }
 
