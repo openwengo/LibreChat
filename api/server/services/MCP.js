aa5aa730b4e5effceca7f78d3762514ca1df1d9f
Olivier Schiavo <olivier.schiavo@wengo.com>
2025-10-10 16:32:41 +0200

    feat: oauth tokens stored in aws secrets / parameters  or mongodb

--- api/server/services/MCP.js
+++ api/server/services/MCP.js
@@ -28,8 +28,8 @@ const {
   getFlowStateManager,
   getMCPManager,
 } = require('~/config');
-const { findToken, createToken, updateToken } = require('~/models');
 const { getGraphApiToken } = require('./GraphTokenService');
+const { getTokenStoreMethods } = require('~/server/services/TokenStore');
 const { reinitMCPServer } = require('./Tools/mcp');
 const { getAppConfig } = require('./Config');
 const { getLogStores } = require('~/cache');
@@ -595,11 +595,7 @@ function createToolInstance({
         requestBody: config?.configurable?.requestBody,
         customUserVars,
         flowManager,
-        tokenMethods: {
-          findToken,
-          createToken,
-          updateToken,
-        },
+        tokenMethods: getTokenStoreMethods(),
         oauthStart,
         oauthEnd,
         graphTokenResolver: getGraphApiToken,
